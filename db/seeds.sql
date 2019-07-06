1813dd7ea8011db30797b980e5f8bc113c16b9bb
Chenla <chenla24@gmail.com>
2019-07-05 21:42:23 -0400

    Working App

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+--Seeds file to populate database with some starter data--
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheese Burger", true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Burger", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Black Bean Burger", true);
